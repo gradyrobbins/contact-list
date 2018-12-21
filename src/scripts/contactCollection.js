64ecca4812f8b2265af1bcf9a8f7ec18d1bc3d09
Grady Robbins <gradyrobbins13@gmail.com>
2018-12-21 09:23:29 -0600

    functional fetch using sample data from 8088 to POSTMAN only

--- src/scripts/contactCollection.js
+++ src/scripts/contactCollection.js
@@ -5,41 +5,22 @@
     Purpose: A ContactCollection component that loads existing contacts from storage, and saves new ones. Each new contact should have an auto-generated identifier.
 */
 
-//getAllFoods() fetch to API
-// post New Food
-
-// const foodCollection = {
-//     getAllFoods() => {
-//         return fetch("http://localhost:8088/fridge")
-//         .then(response => response.json())
-//     }
-
-
-
-// }
-
 //API endpoint:  http://localhost:8088/contactList
 
-// export default contactCollection
+const contactCollection = {
+        getContactList() {
+        return fetch("http://localhost:8088/contacts")
+        .then(response => response.json())
+        },
+        saveContact (contactToSave) {
+           return fetch("http://localhost:8088/contacts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contactToSave)
+            });
+        },
 
-
-//from C29/lego
-// const data = {
-//     postLego (legoToSave) {
-//       fetch("http://localhost:8088/legos", { // Replace "url" with your API's URL
-//       method: "POST",
-//       headers: {
-//           "Content-Type": "application/json"
-//       },
-//       body: JSON.stringify(legoToSave)
-//       })
-//     }
-//     // deleteLego (legoId) {
-//     //   fetch(`http://localhost:8088/legos/${legoId}`, {
-//     //   method: "DELETE",
-//     //   headers: {
-//     //       "Content-Type": "application/json"
-//     //   }
-//     //   })
-//     // }
-//   };
+};
+export default contactCollection
